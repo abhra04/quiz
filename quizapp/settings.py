054a9bc8cc0639af3de952da7b822e230052e4cf
unknown <abhranildas@iitkgp.ac.in>
2021-07-06 23:04:32 +0530

    added postgres

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'quizapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizapp',
+        'USER':'postgres',
+        'PASSWORD' : 'Abhra@20041999',
+        'HOST' : 'localhost',
+        'PORT' : '5432' 
+
     }
 }
 
